80c28b9a574897738c7d6fa4cfb4669f2f2222d3
Anatoly Efremov <anatolefrem1980@gmail.com>
2023-06-19 23:29:46 +0300

    Mentor's remarks taken into account. 1.When invalid numbers are entered, the application should throw an exception and exit. 2.When you enter a string that does not correspond to the operations described in the job, the application should throw an exception and exit.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,36 +3,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         while (true) {
-            //System.out.println(isException3(new Scanner(System.in).nextLine()));
-            System.out.println(calc(new Scanner(System.in).nextLine()));
+             System.out.println(calc(new Scanner(System.in).nextLine()));
         }
     }
     public static String calc(String input) {
-        if (isCorrectInput(input)) {
+        String str = input.replaceAll(" ","");
+        if (isCorrectInput(str)) {
         int i = 0;
         StringBuilder a = new StringBuilder();
-        a.append(input.charAt(i));
+        a.append(str.charAt(i));
           while (i<2)
-            switch (input.charAt(i+1)) {
+            switch (str.charAt(i+1)) {
                 case '+':String s = a.toString();
-                String s1 = input.substring(i+2);
+                String s1 = str.substring(i+2);
                     return Integer.toString(Integer.parseInt(s) + Integer.parseInt(s1));
                 case '-':
-                    return Integer.toString(Integer.parseInt(a.toString()) - Integer.parseInt(input.substring(i+2)));
+                    return Integer.toString(Integer.parseInt(a.toString()) - Integer.parseInt(str.substring(i+2)));
                 case '/':
-                    return Integer.toString(Integer.parseInt(a.toString()) / Integer.parseInt(input.substring(i+2)));
+                    return Integer.toString(Integer.parseInt(a.toString()) / Integer.parseInt(str.substring(i+2)));
                 case '*':
-                    return Integer.toString(Integer.parseInt(a.toString()) * Integer.parseInt(input.substring(i+2)));
-                default: i++; a.append(input.charAt(i)); //continue;
+                    return Integer.toString(Integer.parseInt(a.toString()) * Integer.parseInt(str.substring(i+2)));
+                default: i++; a.append(str.charAt(i)); //continue;
             }
-        } else if (isException1(input)) {
+        } else if (isException1(str)) {
             throw new RuntimeException("т.к. строка не является математической операцией");
-        } else if (isException2(input)) {
+        } else if (isException2(str)) {
             throw new RuntimeException("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-        } else if (isException3(input)&&!isCorrectInput(input)) {
+        } else if (isException3(str)&&!isCorrectInput(str)) {
             throw new RuntimeException("т.к Калькулятор умеет работать только с целыми числами от 1 до 10");
 
-        }
+        }else {throw new RuntimeException("Данная операция не возможна т.к. не предусмотрена калькулятором");}
         return "Не предвиденная ошибка";
     }
     static Boolean isCorrectInput(String s) {
